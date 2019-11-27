288e49d15a34c1cc98537ca32a56edbaeb048b25
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2019-11-27 22:22:20 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-var autofill = ["absorb","absolute","absent","absence","accompany","acquire","beautiful","bad","bear"];
-var obj = document.getElementById("search");
-obj.addEventListener("keyup",autoFill);
-var len=autofill.length;
-var add = document.getElementById("more");
-function autoFill(){
-	add.innerHTML = "";
-	let val = obj.value.toLowerCase();
-	let number=0;
-	for(let i=0;i<len;i++){
-		if(autofill[i].indexOf(val) == 0 && number < 5){
-			add.innerHTML += '<li class="items">'+autofill[i]+'</li>';
-			number ++;
-		}
-	}
-}
 window.onload = function(){
-	var ajax = new XMLHttpRequest();
-	ajax.open("GET","https://vandung2kk2.github.io/Dictionary/Maps/A.txt",true);
-	ajax.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status == 200) {
-			let arr = JSON.parse(ajax.responseText);
-			console.log(arr,"Van Dung");
+	var autofill;
+	var obj = document.getElementById("search");
+	var add = document.getElementById("more");
+	function getWords(){
+		var ajax = new XMLHttpRequest();
+		ajax.open("GET","https://vandung2kk2.github.io/Dictionary/Maps/A.txt",true);
+		ajax.onreadystatechange = function(){
+			if (this.readyState == 4 && this.status == 200) {
+				autofill = JSON.parse(ajax.responseText);
+			}
+		}
+		ajax.send();
+	}
+	obj.addEventListener("keyup",autoFill);
+	function autoFill(){
+		if(!autofill) getWords();
+		var len=autofill.length;
+		add.innerHTML = "";
+		let val = obj.value.toLowerCase();
+		let number=0;
+		for(let i=0;i<len;i++){
+			if(autofill[i].indexOf(val) == 0 && number < 5){
+				add.innerHTML += '<li class="items">'+autofill[i]+'</li>';
+				number ++;
+			}
 		}
 	}
-	ajax.send();
 }
