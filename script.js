bc69596d986b98505482785d00337795c9c0a434
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2019-11-27 22:02:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var autofill = ["absorb","absolute","absent","absence","accompany","acquire","beautiful","bad","bear"];
+var obj = document.getElementById("search");
+obj.addEventListener("keyup",autoFill);
+var len=autofill.length;
+var add = document.getElementById("more");
+function autoFill(){
+	add.innerHTML = "";
+	let val = obj.value.toLowerCase();
+	let number=0;
+	for(let i=0;i<len;i++){
+		if(autofill[i].indexOf(val) == 0 && number < 5){
+			add.innerHTML += '<li class="items">'+autofill[i]+'</li>';
+			number ++;
+		}
+	}
+}
+var ajax = new XMLHttpRequest();
+ajax.onreadystatechange = function(){
+	if (this.readyState == 4 && this.status == 200) {
+		console.log(this.responseText);
+	}
+}
+ajax.open("GET","http://vandung.com",true);
+ajax.send();
