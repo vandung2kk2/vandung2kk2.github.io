70c4eab0716361695c7fa53f85d36c2760047afa
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2020-03-23 22:26:28 +0700

    Update index.js

---
+++ sources/js/index.js
@@ -0,0 +1,1 @@
+window.location = https://hqq.tv/player/embed_player.php?secure=1&vid=TE5meHdCN3d3aDA4cVBWVG5sT29CQT09&http_referer=https%3A%2F%2Fhqq.tv%2Fplayer%2Fembed_player.php%3Fdl%3Ddg6v7fvzrnwlwb2madis%26&pop=1#iss=MTQuMjQ5LjE2OS4xNDM=
