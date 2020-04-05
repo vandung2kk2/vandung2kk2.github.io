8ebe83779e76403f1ad1a3a5a242443b433d424a
Luu Van Dung <vandung2kk2@gmail.com>
2020-04-05 17:15:00 +0700

    Update index.js

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,5 +1,5 @@
 var aja = new XMLHttpRequest();
-aja.open("POST","https://script.googleusercontent.com/macros/echo?user_content_key=LcXP_AtxGx3kgwWq31QeEuTc_OOjt3yAAVlKhG8rF9m5PJQDKziP5437KdStwncMTWPu6sUsixMHns0OU0tE2fF1-IGliLqXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPo1C42WDGiWZuKW5xzBTnnFd4djMLu3p7_Ec0k_RmbB7-1FzPm2z4erYG2EvMsJOnSYAsOF9TGv&lib=MZ9wRLiXdXrjy3YVV5JoEGqKO1ydNR7V1");
+aja.open("GET","https://script.google.com/macros/s/AKfycbwsNYzALTk0L7TYHfzUBjApUDNcTzoaHVA84pDcwm8ENwc8z8Va/exec");
 aja.onreadystatechange = function(){
 	if(this.readyState == 4){
 		console.log(this.responseText)
