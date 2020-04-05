3d473f8158ed378052ac517db0063fd37033b19f
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2020-04-05 16:40:57 +0700

    Update index.js

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,8 +1,8 @@
 var aj = new XMLHttpRequest();
-aj.open("POST","https://script.google.com/macros/s/AKfycbwWpzghQmjWaZ3LWJRrPdKiL0IQCeogoKDc50vXBogAMv22Dyc/exec", true);
-aj.onreadystatechange = function(){
+aja.open("POST","https://script.google.com/macros/s/AKfycbwWpzghQmjWaZ3LWJRrPdKiL0IQCeogoKDc50vXBogAMv22Dyc/exec", true);
+aja.onreadystatechange = function(){
 	if(this.readyState == 4){
 		console.log(this.responseText)
 	}
 }
-aj.send("Dung");
+aja.send("Dung");
