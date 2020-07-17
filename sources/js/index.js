7d9e00a15527fdcd1d5d22067255f0515ba1d73a
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2020-07-17 16:28:57 +0700

    Delete index.js

--- sources/js/index.js
+++
@@ -1,8 +0,0 @@
-var aja = new XMLHttpRequest();
-aja.open("GET","https://script.google.com/macros/s/AKfycbwsNYzALTk0L7TYHfzUBjApUDNcTzoaHVA84pDcwm8ENwc8z8Va/exec");
-aja.onreadystatechange = function(){
-	if(this.readyState == 4){
-		console.log(this.responseText)
-	}
-}
-aja.send("Dung");
