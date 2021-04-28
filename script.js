a46a9ee9def562b1ecd3bbfd72de28035031e1bc
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-04-28 17:10:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-var words = document.getElementById("words"),
-	meanings = document.getElementById("meanings");
-var dictionary = new XMLHttpRequest();
-window.onload = function(){
-	let wordsList = [];
-	let pos = 0;
-	let status = false;
-	function wordsCreator(list){
-		let res = [];
-		let len = list.length;
-		for(let i=0;i<len;i++){
-			res[i] = list[Math.floor(Math.random()*len)];
+var obj=document.getElementsByClassName("opt");
+for(var i=0; i<obj.length; i++){
+	obj[i].addEventListener("click",function(){
+		for(var i=0; i<obj.length; i++){
+			obj[i].classList.remove("active");
 		}
-		return res;
-	}
-	dictionary.open("GET","https://vandung2kk2.github.io/sources/dictionary/flashcards.txt");
-	dictionary.onreadystatechange = function(){
-		if(this.readyState == 4){
-			wordsList = wordsCreator(JSON.parse(this.responseText).words);
-			status = true;
-			showWord();
-			document.addEventListener("click",showWord)
-		}
-	}
-	dictionary.send();
-	function showWord(){
-		if(status){
-			words.innerText = wordsList[pos];
-                        pos++
-		}
-	}
+		this.classList.add("active");
+	})
 }
